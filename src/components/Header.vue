3e22d730972185b42b56a6412361b765572e5c54
Felix Stein <felix.stein.vb@gmail.com>
2020-01-14 09:40:06 +0100

    fixed header link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
+      <a class="navbar-item" href="/">
         <img
           src="../assets/logo.png"
         />
