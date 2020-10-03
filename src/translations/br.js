b97e40ac9710e45b819eaa5b5dc86579996e0085
Igor M. Nogueira <65968105+odfigor@users.noreply.github.com>
2020-10-03 13:39:30 -0300

    Brazillian Portuguese translation done.
    I concluded the translation.

--- src/translations/br.js
+++ src/translations/br.js
@@ -2,13 +2,13 @@ export default {
   //
   //  Local name of the language
   //
-  languageLocalName: 'BrazillianPortuguese',
+  languageLocalName: 'Brazillian Portuguese',
 
   //
   // General
   //
   title: '3D Code Generator',
-  subtitle: 'Export QR codes or Spotify codes as STL for 3D printing // Exporte QR code ou Spotify codes como STL para impressão 3D',
+  subtitle: 'Exporte QR codes ou Spotify codes como STL para impressão 3D',
   preview: 'Visualizar',
   controlsHint: 'Use seu mouse para rotacionar',
   changeLanguage: 'Mudar Idioma',
@@ -101,89 +101,92 @@ export default {
   style: 'Estilo',
   shape: 'Forma',
   rectangle: 'retangulo',
-  roundedRectangle: 'retangulo arredondado', // PARTE #1
-  square: 'square',
-  round: 'round',
-  size: 'Size',
+  roundedRectangle: 'retangulo arredondado',
+  square: 'quadrado',
+  round: 'círculo',
+  size: 'Tamanho',
   blockSizeHelp: `
-  This settings modifies the size of the individual QR code blocks.
-  Play around with this value to achieve unique visual looks but keep in mind that this could impact readability of the QR code.\\
-  Check the preview with your phone before printing to see if you have gone too far.
-  Stay at 100% if you are not sure.
-  If you increase this value above 100% (e.g. 120%) the blocks will form connected islands that make the QR code easier to print.`,
-  icon: 'Icon',
-  noIcon: 'No icon',
+  Esta configuração modifica o tamanho dos blcos individuais do QR code.
+  Brinque com este valor para atingir visuais únicos mas tenha em mente que isto pode impactar na leitura ou não do QR code.\\
+  Cheque a visualização com o seu celular antes de imprimir para ver se você não está indo muito longe.
+  Deixe em 100% se você não estiver 100% certo.
+  Se você subir este valor acima de 100% (ex: 120%) os blocos vão formar ilhas conectadas para fazer o QR code mais fácil de imprimir`,
+  icon: 'Ícone',
+  noIcon: 'Sem Ícone',
   iconSizeHelp: `
-  The size of the icon relative to the total width of the QR Code.
-  The icon abuses the inbuilt error correction of the qr code. If it is too big the code may not be readable.
-  If you want to have a big icon but your phone can't read the QR code you can try to increase the Error Correction Level.`,
-  text: 'Text',
-  textOnEdge: 'Add a custom title text to your QR code.',
-  placement: 'Placement',
-  theText: 'The text',
+  O tamanho do ícone relativo a largura do QR Code.
+  O ícone abusa da correção de erros embutida do qr code. Se for muito grande o código pode ficar ilegível.
+  Se você quiser um ícone grande, mas seu telefone não puder ler o QR code você pode tentar aumentar o nível de correção de erro.`,
+  text: 'Texto',
+  textOnEdge: 'Adicionar um título personalizado ao seu QR code',
+  placement: 'Colocação',
+  theText: 'Texto',
   cityMode: 'QR-City',
-  cityModeText: 'Randomly alters the height of the blocks.',
-  invert: 'Invert',
-  invertText: 'Inverts the structure of the code',
-  keychain: 'Keychain',
-  keychainHelp: 'Attach hole to the side of the tag (e.g. to attach it to your keychain).',
-  mirrorHoles: 'Mirror Holes',
-  mirrorHolesHelp: 'Mirror the holes to the opposite side (e.g. to fix it with screws).',
-  keychainHoleDiameter: 'Hole Diameter',
+  cityModeText: 'Altera aleatoriamente a altura dos blocos',
+  invert: 'Inverter',
+  invertText: 'Inverte a estrutura do QR code',
+  keychain: 'Chaveiro',
+  keychainHelp: 'Insere um buraco do lado da tag (ex: para usar como chaveiro).',
+  mirrorHoles: 'Espelhar Buracos',
+  mirrorHolesHelp: 'Espelha os buracos para o lado oposto (ex: para pendurar com parafusos).',
+  keychainHoleDiameter: 'Diâmetro do furo',
 
   //
   // Export Settings
   //
-  exportTypeHelp: 'Just leave this as "binary" to keep file size low. If your software has issues with the generated file, you can try to change this option.',
-  exportSeparatePartsHelp: 'If set to "yes" the base and the qr code will be saved as two separate parts for printers with dual extrusion printing. Your browser may ask for permissions to download multiple files.',
-  separateParts: 'Separate parts',
-  saveAsButton: 'Save as STL',
+  exportTypeHelp: 'Simplesmente deixe isso como "Binário" para manter o arquivo pequeno. Se seu software tiver problemas com o arquivo gerado, você pode tentar mudar esta opção',
+  exportSeparatePartsHelp: 'Se você colocar "sim" a base e o qr code vão ser salvos em partes separadas para impressoras com extrusão dupla. Seu navegador pode pedir permissão para baixar múltiplos arquivos',
+  separateParts: 'Separar as partes',
+  saveAsButton: 'Salvar como STL',
 
   //
   // Print Guide
   // with HTML Tags included!
   //
-  printGuideTitle: '3D Print Guide',
-  printGuideSubtitle: 'How to print a dual color QR code with a single extruder 3d printer?',
-  printGuideWIPInfo: 'This guide is a work in progress.',
+  printGuideTitle: 'Guia de impressão 3D',
+  printGuideSubtitle: 'Como imprimir um QR code de duas cores com uma impressora de extrusão simples?',
+  printGuideWIPInfo: 'Este guia é um trabalho em progresso.',
   printGuideIntro: `
-  You can print multi colored objects even with a single extruder by swapping the filament at specific layers.<br/>
-  We can use this method to print the base of our QR code and the actual QR code part on the top in two different colors.<br/>
-  This technique is what makes 3d printable QR codes possible in the first place.<br/>
-  The process is different depending on the slicer software you are using.<br/>
-  In this guide I will focus on Cura and PrusaSlicer only and I am taking no responsibility whatsoever if you somehow damage your printer in the process.<br/>`,
-  printGuideSupportWarningTitle: 'Keep in mind: Not all printers/firmwares support the necessary functionality!',
+  Você pode imprimir objetos multicoloridos mesmo com uma extrusora simples trocando o filamento em camadas específicas.<br/>
+  Nós usamos este método para imprimir a base do seu QR code e o QR code em si no topo com duas cores diferentes.<br/>
+  Esta técnica e o que faz os QR codes imprimíveis possíveis para começo de conversa.<br>
+  Este processo é diferente dependendo do fatiador que você está usando.<br/>
+  Neste guia, Eu vou focar no Cura e no PrusaSlicer, e Não assumo nenhuma responsabilidade se você causar algum dano a sua impressora no processo<br/>`,
+  printGuideSupportWarningTitle: 'Tenha em mente que nem todas as impressoras/firmwares suportam a funcionalidade necessária!',
   printGuideSupportWarningMessage: `
-  This is meant as a general guide, as I can't provide a writeup on every printer/firmware combination out there.<br/>
-  I recommend doing a small test print first. If you are having problems getting this to work, please search online if your specific printer model supports the <strong>M600</strong> G-Code command for filament changing.<br/>`,
-  printGuideGenerateQRCode: 'Generating the QR code',
+  Este é um guia genérico, uma vez que não posso fornecer o guia para todas as impressoras/firmwares que existem.<br/>
+  Eu recomendo fazer um pequeno teste de impressão primeiro. Se você estiver tento problemas para fazer isto funcionar, por favor procure online se seu modelo de impressora específico suporte o comando G-Code <strong>M600</strong> para troca de filamento`,
+  printGuideGenerateQRCode: 'Gerando o QR code',
   printGuideGenerateQRCodeSteps: `
-  <li>Select the type of QR code you want to generate under "QR Code Options".</li>
-  <li>Fill out the necessary fields.</li>
-  <li>Configure the 3d model under "3D Model Options".</li>
-  <li>Click on "Generate 3D Model"</li>
-  <li>Save the stl file via the "Save As STL" button in the top right.</li>`,
-  printGuideVersionDisclaimer: 'Version {version}, your experience can differ.',
+  <li>Escolha o tipo de QR code que quer gerar em "Opções do QR Code"</li>
+  <li>Preencha os campos necessários</li>
+  <li>Configure o modelo 3D em "Opções do Modelo 3D"</li>
+  <li>Clique em "Gerar Modelo 3D"</li>
+  <li>Salve o arquivo stl no botão "Salvar Como STL" no canto superior direito</li>`,
+  printGuideVersionDisclaimer: 'Versão {version}, você poderá ter uma experiencia diferente',
   // Cura
   printGuideCuraStep1: `
-  Slice the model and locate the layer where the color change should happen.<br/>
-  In my case this is at layer 16.<br/>`,
+  Fatie o modelo e localize a camada onde a troca de cor poderia acontecer<br/>
+  No meu caso, isso foi na camada 16.<br/>`,
   printGuideCuraStep2: `
-  <li>Go to "Extensions -> Post Processing -> Modify G-Code".</li>
-  <li>Click on "Add a script" then select "Filament Change".</li>
-  <li>In the Filament Change settings, set the "Layer" value to your layer number from step 1.</li>
-  <li>Re-Slice your model. The icon left of the "Slice" button indicates an active Post Processing script.</li>`,
+  <li>Vá para "Extensões -> Pós-Processamento -> Modificar G-Code</li>
+  <li>Clique em "Adicionar um Script" então selecione "Troca de Filamento".</li>
+  <li>Nas opções de troca de filamento, coloque em "Camada" o número de camada do passo 1</li>
+  <li>Fatie novamente seu modelo. O ícone a esquerda do botão "Fatiar" indica um script de Pós Processamento ativo</li>`,
   // PrusaSlicer
   printGuidePrusaSlicerStep1: `
   Slice the model and locate the layer where the color change should happen.<br/>
-  In my case this is at layer 11.<br/>`,
+  In my case this is at layer 11.<br/>
+
+  Fatie o modelo e localize a camada onde a troca de cor poderá acontecer.<br/>
+  No meu caso, isso foi na camada 11<bnr/>`,
   printGuidePrusaSlicerStep2: `
-  <li>Click on the little plus sign right of the layer selection bar.</li>
-  <li>PrusaSlicer gives you a nice preview where you can see the different colors to verify that you selected the right layer. The qr code parts should have a different color than the base</li>
-  <li>Re-Slice your model.</li>`,
+  li>Clique no pequeno ícone de adição na barra de seleção de camada.</li>
+  <li>O PrusaSlicer vai te fornecer uma boa prévia onde você poderá ver as cores diferentes para veificar se você selecionou a camada certa. As partes do qr code devem ter uma cor diferente da base</li>
+  <li>Fatie novamente seu modelo.</li>`,
   printGuideStep3: `
-  You can now print the model as usual.<br/>
-  The 3d printer will pause on the specified layer and move to the origin of the print bed.
-  Now you can swap the filament and restart the print job from your printers menu.`,
+  Agora, você pode imprimir o modelo como de costume<br/>
+  Sua impressora 3D irá pausar na camada especificada e mover-se para a origem da mesa de impressão.
+  Agora, você poderá trocar o filamento e reiniciar o processo de impressão no menu da sua impressora.`,
 
 };
