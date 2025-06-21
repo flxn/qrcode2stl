1340ced0f74fb62ecc67fe6cf8742a724cd409f7
Felix Stein <mail@flxn.de>
2025-06-21 14:18:12 +0200

    fix: changed base URL configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/qrcode2stl/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
